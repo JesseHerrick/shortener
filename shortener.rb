3ae321e4166e7cdfc8042da94d6de828d877c8e3
Jesse Herrick <jessegrantherrick@gmail.com>
2013-11-03 01:00:27 -0400

    Add the post / and get :code code.

--- shortener.rb
+++ shortener.rb
@@ -3,6 +3,7 @@ require 'redis'
 require 'digest/md5'
 
 set :environment, :production
+db = Redis.new
 
 helpers do
 	include Rack::Utils  
@@ -21,4 +22,28 @@ end
 
 get '/' do
 	erb :index
+end
+
+post '/' do
+	# params[:url] is the url param that has the value of the given longUrl.
+	@url = params[:url]
+	# If the URL doesn't have a protocal, add it.
+	unless @url.include?("https://") || @url.include?("http://")
+		@url = "http://" + @url
+	end
+	# If the URL exists and isn't empty then md5/hash it and use the first 6 chars as the code.
+	if @url and not @url.empty?
+		# Creates a hash of the given longUrl.
+		@code = hash(@url)
+		@code = shorten(@code, 0..5)
+		puts "Code: #{@code}"
+		db.setnx("link:#{@code}", @url)
+	end
+	# Then go back to '/'
+	erb :index
+end
+
+get '/:code' do
+	@url = db.get("link:#{params[:code]}")
+	redirect @url || '/'
 end
